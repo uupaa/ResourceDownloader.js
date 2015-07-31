1f6a2b4717e5d6525c255ffc635cc0ef96d407e4
uupaa <uupaa.js@gmail.com>
2015-08-01 07:51:00 +0900

    add remove() and clear()

--- test/testcase.js
+++ test/testcase.js
@@ -1,6 +1,7 @@
 var ModuleTestResourceDownloader = (function(global) {
 
 global["BENCHMARK"] = false;
+//ResourceDownloader["VERBOSE"] = true;
 
 var test = new Test("ResourceDownloader", {
         disable:    false, // disable all tests.
@@ -19,6 +20,12 @@ var test = new Test("ResourceDownloader", {
         // generic test
         testResourceDownloader_200,
         testResourceDownloader_404,
+        testResourceDownloader_remove,
+        testResourceDownloader_clear,
+        testResourceDownloader_parallel_1,
+        testResourceDownloader_parallel_2,
+        testResourceDownloader_parallel_3,
+        testResourceDownloader_parallel_100,
     ]);
 
 if (IN_BROWSER || IN_NW) {
@@ -37,7 +44,6 @@ if (IN_BROWSER || IN_NW) {
 
 // --- test cases ------------------------------------------
 function testResourceDownloader_200(test, pass, miss) {
-
     var dl = new ResourceDownloader();
 
     dl.ontick = function() {
@@ -56,12 +62,9 @@ function testResourceDownloader_200(test, pass, miss) {
                  "./nw.html",
                  "./package.json",
                  "./worker.js"];
-
 }
 
 function testResourceDownloader_404(test, pass, miss) {
-
-
     var dl = new ResourceDownloader();
 
     dl.ontick = function() {
@@ -78,7 +81,136 @@ function testResourceDownloader_404(test, pass, miss) {
                  "./nw.xx.html",
                  "./package.xx.json",
                  "./worker.xx.js"];
+}
 
+function testResourceDownloader_remove(test, pass, miss) {
+    var url = "../lib/ResourceDownloader.js";
+    var dl = new ResourceDownloader();
+
+    dl.ontick = function() {
+        if ( Object.keys(dl.source).length === 7 && dl.remain === 0 ) {
+            dl.remove(url);
+            if ( Object.keys(dl.source).length === 6 && dl[url] === undefined) {
+                test.done(pass());
+                return;
+            }
+            test.done(miss());
+        }
+    };
+    dl.onerror = function(error) {
+        test.done(miss());
+    };
+
+    dl.source =  url;
+    dl.source = ["../lib/WebModule.js",
+                 "./index.html",
+                 "./node.js",
+                 "./nw.html",
+                 "./package.json",
+                 "./worker.js"];
+}
+
+function testResourceDownloader_clear(test, pass, miss) {
+    var url = "../lib/ResourceDownloader.js";
+    var dl = new ResourceDownloader();
+
+    dl.ontick = function() {
+        if ( Object.keys(dl.source).length === 7 && dl.remain === 0 ) {
+            dl.clear();
+            if ( Object.keys(dl.source).length === 0 && dl[url] === undefined) {
+                test.done(pass());
+                return;
+            }
+            test.done(miss());
+        }
+    };
+    dl.onerror = function(error) {
+        test.done(miss());
+    };
+
+    dl.source =  url;
+    dl.source = ["../lib/WebModule.js",
+                 "./index.html",
+                 "./node.js",
+                 "./nw.html",
+                 "./package.json",
+                 "./worker.js"];
+}
+
+function testResourceDownloader_parallel_1(test, pass, miss) {
+    var dl = new ResourceDownloader(1);
+
+    dl.ontick = function() {
+        test.done(pass());
+    };
+    dl.onerror = function(error) {
+        test.done(miss());
+    };
+
+    dl.source = ["../LICENSE",
+                 "../lib/WebModule.js",
+                 "./index.html",
+                 "./node.js",
+                 "./nw.html",
+                 "./package.json",
+                 "./worker.js"];
+}
+
+function testResourceDownloader_parallel_2(test, pass, miss) {
+    var dl = new ResourceDownloader(2);
+
+    dl.ontick = function() {
+        test.done(pass());
+    };
+    dl.onerror = function(error) {
+        test.done(miss());
+    };
+
+    dl.source = ["../LICENSE",
+                 "../lib/WebModule.js",
+                 "./index.html",
+                 "./node.js",
+                 "./nw.html",
+                 "./package.json",
+                 "./worker.js"];
+}
+
+function testResourceDownloader_parallel_3(test, pass, miss) {
+    var dl = new ResourceDownloader(3);
+
+    dl.ontick = function() {
+        test.done(pass());
+    };
+    dl.onerror = function(error) {
+        test.done(miss());
+    };
+
+    dl.source = ["../LICENSE",
+                 "../lib/WebModule.js",
+                 "./index.html",
+                 "./node.js",
+                 "./nw.html",
+                 "./package.json",
+                 "./worker.js"];
+}
+
+function testResourceDownloader_parallel_100(test, pass, miss) {
+    var dl = new ResourceDownloader(100);
+
+    dl.ontick = function() {
+        test.done(pass());
+    };
+    dl.onerror = function(error) {
+        test.done(miss());
+    };
+
+    dl.source = ["../README.md",
+                 "../lib/WebModule.js",
+                 "./index.html",
+                 "./node.js",
+                 "./nw.html",
+                 "./package.json",
+                 "./worker.js"];
 }
 
 return test.run();
